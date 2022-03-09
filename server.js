93c797ef5d1d241ba798de1bd981dc5ce7e3868b
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-09 15:46:19 -0600

    connected to databse

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const mysql = require('mysql2');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "election"
+    },
+    console.log('Connected to the election database.')
+)
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
