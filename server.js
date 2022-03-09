9b743bde2b5cfec13e49ef652865dfbaeaf062ee
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-09 16:32:34 -0600

    delete request working

--- server.js
+++ server.js
@@ -36,13 +36,27 @@ app.get('/api/candidate/:id', (req, res) => {
     })
 });
 
-// // delete a candidate
-// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-//     if (err) {
-//         console.log(err);
-//     }
-//     console.log(result);
-// });
+// delete a candidate
+app.delete("/api/candidate/:id", (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "candidate not found"
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    })
+});
 
 // // create a candidate
 // const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
