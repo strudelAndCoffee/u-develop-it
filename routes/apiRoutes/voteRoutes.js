0942272568f5bc6d46dceec7bf329be033a960b4
Bort <93967399+strudelAndCoffee@users.noreply.github.com>
2022-03-10 23:48:26 -0600

    added api route to get all votes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -28,4 +28,23 @@ router.post("/vote", ({ body }, res) => {
     });
 });
 
+// get all votes for candidates in descending order of votes
+router.get("/votes", (req, res) => {
+    const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                    FROM votes
+                    LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                    LEFT JOIN parties ON candidates.party_id = parties.id
+                    GROUP BY candidate_id ORDER BY count DESC;`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
+        res.json({
+            message: "success",
+            data: rows
+        });
+    });
+});
+
 module.exports = router;
